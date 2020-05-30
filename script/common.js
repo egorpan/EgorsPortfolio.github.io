2fe9ad8e08ed68a90604706409b9d6caf3325251
egorpan <66021228+egorpan@users.noreply.github.com>
2020-05-31 01:27:55 +0500

    SCRIPTS

---
+++ script/common.js
@@ -0,0 +1,12 @@
+alert('Hi')
+class Man{
+  _test = 5
+  constructor(x){
+    
+  }
+}
+
+
+const testestestest = 5;
+
+console.log(testestestest)
---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+(()=>{
+  let test = 0
+  class TEST{
+    constructor(x){
+
+    }
+  }
+})
